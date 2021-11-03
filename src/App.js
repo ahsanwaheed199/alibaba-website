4a3436447be04fc9aca5696ddc85b26edd16f4a3
Ahsan Awan <60344115+ahsanwaheed199@users.noreply.github.com>
2021-11-03 21:24:58 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import DenseAppBar from './components/Titles';
+import Body from './components/Body';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Header />
+    <DenseAppBar />
+    <Body></Body>
     </div>
   );
 }
